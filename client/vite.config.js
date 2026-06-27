a7870990caac1defe9b103665f964fa067d69b61
nirakarpatel <you@example.com>
2026-06-28 01:06:41 +0530

    UI: Update ClaimUsername to use Shadcn Select

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import analyzer from 'vite-bundle-analyzer'
+import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -20,6 +21,11 @@ export default defineConfig({
     strictPort: true,
     headers: {
       'Cross-Origin-Opener-Policy': 'same-origin-allow-popups',
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
   },
   build: {
